0d25131762119d10515bbe0af279e484994882ac
bryson <brysonkaranja@gmail.com>
2023-05-16 10:54:54 +0300

    Initial Commit

---
+++ War card game/War_card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_card_gameApp.swift
+//  War card game
+//
+//  Created by Bryson Karanja on 16/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct War_card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
